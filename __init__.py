9e6b58c11ac292a721900d3a2d734b43befb23df
Sebastian Schneider <sschneider@point-8.de>
2019-08-05 01:28:49 +0200

    uncomment everything but the dash layout for testing.

--- __init__.py
+++ __init__.py
@@ -18,12 +18,12 @@ from helpers import get_dash_dropdown_options
 
 PAGE_SIZE = 10
 VISIBLE_COLUMNS = ['competition', 'year', 'matchday', 'team', 'name', 'goal_difference', 'duration']
-df_datasets = pd.read_pickle('./data/df_datasets.pkl')
-df_datasets = df_datasets.dropna(subset=['goal_difference'])
-competitions = df_datasets['competition'].unique()
-competition_options = get_dash_dropdown_options(competitions, competitions)
-seasons = df_datasets['year'].unique()
-season_options = get_dash_dropdown_options(seasons, seasons)
+#df_datasets = pd.read_pickle('./data/df_datasets.pkl')
+#df_datasets = df_datasets.dropna(subset=['goal_difference'])
+#competitions = df_datasets['competition'].unique()
+#competition_options = get_dash_dropdown_options(competitions, competitions)
+#seasons = df_datasets['year'].unique()
+#season_options = get_dash_dropdown_options(seasons, seasons)
 
 #external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash()
@@ -41,8 +41,8 @@ app.layout = html.Div(children=[
             sort_mode="multi",
         ),
     ),
-    dcc.Dropdown(id='dropdown-competition', options=competition_options, value='Bundesliga'),
-    dcc.Dropdown(id='dropdown-season', options=season_options, value='2018-19'),
+    #dcc.Dropdown(id='dropdown-competition', options=competition_options, value='Bundesliga'),
+    #dcc.Dropdown(id='dropdown-season', options=season_options, value='2018-19'),
     dcc.Dropdown(id='dropdown-team'),
     dcc.Dropdown(id='dropdown-player'),
     html.Div('C 2'),
@@ -51,92 +51,92 @@ app.layout = html.Div(children=[
 ])
 
 
-@app.callback(
-    Output('datatable-raw', 'data'),
-    [Input('dropdown-competition', 'value'),
-     Input('dropdown-season', 'value'),
-     Input('dropdown-team', 'value'),
-     Input('dropdown-player', 'value'),
-     ]
-)
-def update_datatable_raw(competition, season, team, player_url):
-    df = df_datasets
-    if competition:
-        df = get_competition_df(df, competition)
-    if season:
-        df = get_season_df(df, season)
-    if team:
-        df = get_team_df(df, team)
-    if player_url:
-        df = get_player_df(df, player_url)
-    return df[VISIBLE_COLUMNS].to_dict('records')
+# @app.callback(
+#     Output('datatable-raw', 'data'),
+#     [Input('dropdown-competition', 'value'),
+#      Input('dropdown-season', 'value'),
+#      Input('dropdown-team', 'value'),
+#      Input('dropdown-player', 'value'),
+#      ]
+# )
+# def update_datatable_raw(competition, season, team, player_url):
+#     df = df_datasets
+#     if competition:
+#         df = get_competition_df(df, competition)
+#     if season:
+#         df = get_season_df(df, season)
+#     if team:
+#         df = get_team_df(df, team)
+#     if player_url:
+#         df = get_player_df(df, player_url)
+#     return df[VISIBLE_COLUMNS].to_dict('records')
 
 
-@app.callback(
-    Output('graph-season-overview', 'figure'),
-    [Input('dropdown-competition', 'value'),
-     Input('dropdown-season', 'value')])
-def update_season_overview(competition, season):
-    if competition and season:
-        return plot_season_overview(df_datasets, competition, season)
-    else:
-        return go.Figure([])
+# @app.callback(
+#     Output('graph-season-overview', 'figure'),
+#     [Input('dropdown-competition', 'value'),
+#      Input('dropdown-season', 'value')])
+# def update_season_overview(competition, season):
+#     if competition and season:
+#         return plot_season_overview(df_datasets, competition, season)
+#     else:
+#         return go.Figure([])
 
 
-@app.callback(
-    Output('graph-team-overview', 'figure'),
-    [Input('dropdown-team', 'value')],
-    [State('dropdown-competition', 'value'),
-     State('dropdown-season', 'value')]
-)
-def update_team_overview(team, competition, season):
-    if team and competition and season:
-        return plot_team_overview(
-            df=df_datasets,
-            team=team,
-            competition=competition,
-            season=season,
-        )
-    else:
-        return go.Figure([])
+# @app.callback(
+#     Output('graph-team-overview', 'figure'),
+#     [Input('dropdown-team', 'value')],
+#     [State('dropdown-competition', 'value'),
+#      State('dropdown-season', 'value')]
+# )
+# def update_team_overview(team, competition, season):
+#     if team and competition and season:
+#         return plot_team_overview(
+#             df=df_datasets,
+#             team=team,
+#             competition=competition,
+#             season=season,
+#         )
+#     else:
+#         return go.Figure([])
 
 
-@app.callback(
-    Output('dropdown-team', 'options'),
-    [Input('dropdown-season', 'value')],
-    [State('dropdown-competition', 'value')]
-)
-def update_dropdown_team_options(season, competition):
-    if season and competition:
-        df = get_competition_df(df_datasets, competition)
-        df = get_season_df(df, season)
-        teams = sorted(df['team'].unique())
-        return get_dash_dropdown_options(teams, teams)
+# @app.callback(
+#     Output('dropdown-team', 'options'),
+#     [Input('dropdown-season', 'value')],
+#     [State('dropdown-competition', 'value')]
+# )
+# def update_dropdown_team_options(season, competition):
+#     if season and competition:
+#         df = get_competition_df(df_datasets, competition)
+#         df = get_season_df(df, season)
+#         teams = sorted(df['team'].unique())
+#         return get_dash_dropdown_options(teams, teams)
 
-    else:
-        return []
+#     else:
+#         return []
 
 
-@app.callback(
-    Output('dropdown-player', 'options'),
-    [Input('dropdown-team', 'value')],
-    [State('dropdown-competition', 'value'),
-     State('dropdown-season', 'value')]
-)
-def update_dropdown_team_options(team, competition, season):
-    if season and competition and team:
-        print(season, competition, team)
-        df = get_competition_df(df_datasets, competition)
-        df = get_season_df(df, season)
-        df = get_team_df(df, team)
-        print(df.head())
-        player_urls, player_names = df[['url', 'name']] \
-            .drop_duplicates().sort_values('name').values.transpose()
-        print(player_urls, player_names)
-        return get_dash_dropdown_options(player_urls, player_names)
+# @app.callback(
+#     Output('dropdown-player', 'options'),
+#     [Input('dropdown-team', 'value')],
+#     [State('dropdown-competition', 'value'),
+#      State('dropdown-season', 'value')]
+# )
+# def update_dropdown_team_options(team, competition, season):
+#     if season and competition and team:
+#         print(season, competition, team)
+#         df = get_competition_df(df_datasets, competition)
+#         df = get_season_df(df, season)
+#         df = get_team_df(df, team)
+#         print(df.head())
+#         player_urls, player_names = df[['url', 'name']] \
+#             .drop_duplicates().sort_values('name').values.transpose()
+#         print(player_urls, player_names)
+#         return get_dash_dropdown_options(player_urls, player_names)
 
-    else:
-        return []
+#     else:
+#         return []
 
 
 server = app.server
